56063d54c082914f3f7e438d74bcffe52e71d48d
Linxue <angelren1220@gmail.com>
2023-09-29 22:46:46 -0400

    add api endpoints for module and lesson

--- courses/urls.py
+++ courses/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CourseViewSet
+from .views import CourseViewSet, ModuleViewSet, LessonViewSet
 
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet)
+router.register(r'modules', ModuleViewSet)
+router.register(r'lessons', LessonViewSet)
 
 urlpatterns = [
   path('api/', include(router.urls)),
--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Course
-from .serializers import CourseSerializer
+from .models import Course, Module, Lesson
+from .serializers import CourseSerializer, ModuleSerializer, LessonSerializer
 
 class CourseViewSet(viewsets.ModelViewSet):
   queryset = Course.objects.all()
   serializer_class = CourseSerializer
+
+class ModuleViewSet(viewsets.ModelViewSet):
+  queryset = Module.objects.all()
+  serializer_class = ModuleSerializer
+
+class LessonViewSet(viewsets.ModelViewSet):
+  queryset = Lesson.objects.all()
+  serializer_class = LessonSerializer
