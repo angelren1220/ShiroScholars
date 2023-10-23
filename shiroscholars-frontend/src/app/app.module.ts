99594cc447a8d24da95933967f5ade9af85a4da5
Linuxe <angelren1220@gmail.com>
2023-10-23 18:13:37 -0400

    add components

--- shiroscholars-frontend/src/app/app.module.ts
+++ shiroscholars-frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import { SchedulerComponent } from './scheduler/scheduler.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CourseListComponent,
+    SchedulerComponent
   ],
   imports: [
     BrowserModule,
